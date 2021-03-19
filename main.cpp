6c35903e1aa9cf489b0ca2beba1a5285fa054c26
Kacyn1 <62652276+Kacyn1@users.noreply.github.com>
2021-03-19 08:55:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,11 +23,11 @@ pliki::~pliki() {
 
 class obraz {
 public:
-    int whichPicture = 0;
-    vector <string> picture;
+    int ktory_obraz = 0;
+    vector <string> obraz;
     void get_obraz(string numbers);
-    void checkPicture();
-    void clearVector();
+    void sprawdz_obraz();
+    void wyczysc_Vector();
 };
 
 void obraz::get_obraz(string numbers) {
@@ -37,7 +37,7 @@ void obraz::get_obraz(string numbers) {
 
 void obraz::checkPicture() {
     int howManyX = 0, howManyY = 0, badX = 0, badY = 0, x, y ;
-    whichPicture++;
+    ktory_obraz++;
     for (int i = 0; i < obraz.size() - 1; i++) {
     for (int j = 0; j < obraz.size() - 1; j++) {
      if (obraz[i][j] == '1') {
@@ -46,42 +46,34 @@ void obraz::checkPicture() {
      if (obraz[j][i] == '1') {
          howManyY++;
  }
-
  }
         if ((howManyX % 2 == 0 && obraz[i][20] == '1') || (howManyX % 2 == 1 && obraz[i][20] == '0')) {
             badX++;
             y = i;
-
  }
-
         if ((howManyY % 2 == 0 && obraz[20][i] == '1') || (howManyY % 2 == 1 && obraz[20][i] == '0')) {
             badY++;
             x = i;
   }
-
         howManyX = 0;
         howManyY = 0;
-
-    }
+  }
     
     if ((badX == 1 && badY == 1)) {
-        cout << "Obrazek nr: " << whichPicture << endl;
+        cout << "Obrazek nr: " << ktory_obraz << endl;
         cout << "Wspolrzedne: " << y + 1 << ", " << x + 1 << endl;
     } else if ((badX == 0 && badY == 1)) {
-        cout << "Obrazek nr: " << whichPicture << endl;
+        cout << "Obrazek nr: " << ktory_obraz << endl;
         cout << "Wspolrzedne: " << "21" << ", " << x + 1 << endl;
     } else if ((badX == 1 && badY == 0)) {
-        cout << "Obrazek nr: " << whichPicture << endl;
+        cout << "Obrazek nr: " << ktory_obraz << endl;
         cout << "Wspolrzedne: " << y+1 << ", " << "21" << endl;
-    }
-    
+    }  
     badX = 0;
-    badY = 0;
-    
+    badY = 0;  
 }
-
-void obraz::clearVector() {
-    obraz.clear();
+void obraz::wyczysc_Vector() {
+obraz.clear();
 }
 
 int main() {
@@ -89,11 +81,11 @@ int main() {
     obraz o;
     string temp;
     while (!f.fIn.eof()) {
-        for (int i = 0; i < 21; i++) {
-            p.fIn >> temp;
-            o.get_obraz(temp);
-        }
-        o.checkPicture();
-        o.clearVector();
-    }
+    for (int i = 0; i < 21; i++) {
+         p.fIn >> temp;
+         o.get_obraz(temp);
+}
+    o.sprawdz_obraz();
+    o.wyczysc_Vector();
+}
 }
